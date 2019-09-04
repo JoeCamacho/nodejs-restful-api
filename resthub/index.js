b849154667955d444fc1879f4421f9df97d1ba20
Joel <camacho-joel@hotmail.com>
2019-09-04 17:08:17 +0200

    setting up default route

--- resthub/index.js
+++ resthub/index.js
@@ -9,3 +9,7 @@ app.get("/", (req, res) => res.send("Hello World with Express"));
 app.listen(port, function() {
   console.log("Running RestHub on port " + port);
 });
+
+let apiRoutes = require("./api-routes");
+
+app.use("/api", apiRoutes);
